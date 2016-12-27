c62eceb9885fb8acde442468485871bde7151f1d
Muneeswar <muni.kmca@gmail.com>
2016-12-27 12:30:49 -0800

    New aem sample project

---
+++ aem-sample-project1/bundle/src/main/java/com/aem/sample/project/HelloService.java
@@ -0,0 +1,13 @@
+package com.aem.sample.project;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
---
+++ aem-sample-project1/bundle/src/main/java/com/aem/sample/project/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.aem.sample.project;
+
+import aQute.bnd.annotation.Version;
